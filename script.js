5763c194b0f3e8ba880f9c0cebcdcabce40a43d5
ahob85 <aaron@ahob85.com>
2021-06-23 16:59:27 +0000

    Aaron's commit

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
 // Author:
 
 // Global UI Variables
-//let canvasDiv;
+let canvasDiv;
+let canvas;
+let textDiv;
+let textP;
+let buttonDiv;
+let uploadButton;
+let submitButton
+let resetButton;
 
 // Global ML Variables
+let mobilenet;
+let img;
 //let mobilenet;
 
 function setup() {
+  cavasDiv = createDiv();
+  canvas = createCanvas(640, 480);
+  canvas.parent(canvasDiv);
+  textDiv =  createDiv();
+  textP = createP("Model loading, please wait...");
+  textP.parent(textDiv);
+  buttonDiv = createDiv();
+  uploadButton = createFileInput(handleFile);
+  uploadButton.parent(buttonDiv);
+  uploadButton.style("display", "none");
+  submitButton = createButton("SUBMIT");
+  submitButton.parent(buttonDiv);
+  submitButton.mousepressed(predictImage);
+  submitButton.style("display", "none");
+  resetButton = createButton("RESET");
+  resetButton.parent(buttonDiv);
+  resetButton.mousePressed(resetCanvas):
+  resetButton.style("display", "none");
 
+  mobilenet = ml5.imageClassifier("mobileNet", modelReady);
 }
 
 function draw() {
+  textP.html("Upload ann image to classify!");
+  uploadButton.style("display", "none");
 
 }
 
@@ -19,6 +49,10 @@ function modelReady() {
 }
 
 function resetCanvas() {
+  background(255);
+  submitButton.style("display", "none");
+  resetButton.style("display", "none");
+  modelReady();
 
 }
 
@@ -31,13 +65,17 @@ function handleFile(file) {
 }
 
 function imageReady() {
-
+  image(img, 0, 0, width, height);
+  submitButton.style("display", "inside");
+  resetButton.style("display", "none");
+  textP.html("Image ready for classification");
 }
 
 function predictImage() {
-
+  mobilenet.classify(canvas, gotResults);
 }
 
 function gotResults(error, results) {
+  if(error)
 
 }
